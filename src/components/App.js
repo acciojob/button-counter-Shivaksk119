3e49a4116b6f487344a28ffa4b73bad403386597
Karru <shivakumar.karru@thermofisher.com>
2024-12-11 00:20:32 +0530

    first attempt

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [state,setState] = useState(0)
+  onClick=(e)=>{
+    let count = state
+    count++
+    setState({state:count})
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button Clicked {state} times</p>
+        <button onClick={onClick}>Click me</button>
     </div>
   )
 }
